5e5ce96f80da0f93616fb2246fdc4b7491723d0c
Prazepam <vincentbriquet237@gmail.com>
2022-03-08 14:31:51 +0100

    liens vers les pages des photographes + retour à l'accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,15 +1,21 @@
 function photographerFactory(data) {
-    const { name, portrait, city, country, tagline, price } = data;
+    const { name, id, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
+        const article = document.createElement('article');
 
-        const img = document.createElement( 'img' );
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
 
-        const h2 = document.createElement( 'h2' );
+        const photographBox = document.createElement('div');
+        photographBox.classList.add('photographBox');
+
+        const portraitBox = document.createElement('div');
+        portraitBox.classList.add('portraitBox');
+
+        const h2 = document.createElement('h2');
         h2.textContent = name;
         h2.setAttribute("arialabel", "Identité du photographe");
 
@@ -27,17 +33,31 @@ function photographerFactory(data) {
         tariff.textContent = price+"€/jour";
         tariff.classList.add('tariff');
         tariff.setAttribute("arialabel", "Tarif du photographe")
+        
+        const link = `http://127.0.0.1:5500/photographer.html?id=${id}`
+
+        const linkProfilPhotograph = document.createElement ('a');
+        linkProfilPhotograph.classList.add('linkProfilPhotograph');
+        linkProfilPhotograph.setAttribute('href', link);
+        linkProfilPhotograph.setAttribute("arialabel", "lien vers le profil du photographe");
 
 
 
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        article.appendChild(linkProfilPhotograph);
+        linkProfilPhotograph.appendChild(photographBox);
+        photographBox.appendChild(portraitBox);
+        portraitBox.appendChild(img);
+        photographBox.appendChild(h2)
         article.appendChild(cityCountry);
         article.appendChild(bio);
         article.appendChild(tariff);
 
+        
+        
+
 
         return (article);
     }
-    return { name, picture, city, country, tagline, price, getUserCardDOM }
+    return { name, id, picture, city, country, tagline, price, getUserCardDOM }
 }
